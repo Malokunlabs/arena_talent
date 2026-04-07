0cb6027333c7314ba23e74ca33a93bc5d1222fe8
Malokunlabs <149055218+Malokunlabs@users.noreply.github.com>
2026-04-07 14:36:03 +0100

    Uncomment API_BASE_URL and update local URL

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,8 @@
-// export const API_BASE_URL =
-//   process.env.NEXT_PUBLIC_API_BASE_URL ||
-//   "https://arena-talent-backend.up.railway.app";
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://arena-talent-backend.up.railway.app";
 
-export const API_BASE_URL = "http://localhost:3000";
+// export const API_BASE_URL = "http://localhost:3000";
 
 export const R2_PUBLIC_URL =
   process.env.NEXT_PUBLIC_R2_PUBLIC_URL || "";
